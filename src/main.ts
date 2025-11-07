1148b51ad783876302aa6309bc5ce4a7632845ae
Maaz <mzperwez786@gmail.com>
2025-11-08 03:23:24 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      "https://scanmycar-frontend.vercel.app",
+      "http://localhost:3000"
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Origin", "https://scanmycar-frontend.vercel.app");
+      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      return res.status(204).send();
+    }
+    next();
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
